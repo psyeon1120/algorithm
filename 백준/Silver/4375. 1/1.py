ba1b5daf9e7cccb3e297466685ce8de0231e7025
Seo Yeon Park <psycsh@naver.com>
2023-08-03 15:56:02 +0900

    [Silver III] Title: 1, Time: 160 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+
+    num = 0
+    digit = 1
+    while True:
+        num = num * 10 + 1
+        if num % n == 0:
+            print(digit)
+            break
+        digit += 1
