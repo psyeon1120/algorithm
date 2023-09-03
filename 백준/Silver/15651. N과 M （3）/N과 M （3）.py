204284799e581b6e5084d79f9e53ff5cef1b108b
Seo Yeon Park <psycsh@naver.com>
2023-09-03 23:21:47 +0900

    [Silver III] Title: N과 M (3), Time: 1424 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def dfs():
+    if len(arr) == M:
+        print(' '.join(map(str, arr)))
+        return
+
+    for i in range(1, N+1):
+        arr.append(i)
+        dfs()
+        arr.pop()
+
+N, M = map(int, input().split())
+arr = []
+
+dfs()
