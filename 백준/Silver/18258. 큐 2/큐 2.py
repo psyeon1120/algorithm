adc7e951f56b5ec98807ccafe02917844c32ffee
Seo Yeon Park <psycsh@naver.com>
2023-09-18 00:58:59 +0900

    [Silver IV] Title: 큐 2, Time: 1620 ms, Memory: 175128 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+
+queue = deque()
+
+for i in range(N):
+    order = input().split()
+
+    if order[0] == "push":
+        queue.appendleft(order[1])
+
+    elif order[0] == "pop":
+        if len(queue) != 0: print(queue.pop())
+        else: print(-1)
+
+    elif order[0] == "size":
+        print(len(queue))
+
+    elif order[0] == "empty":
+        if len(queue) == 0: print(1)
+        else : print(0)
+
+    elif order[0] == "front":
+        if len(queue) == 0: print(-1)
+        else: print(queue[len(queue) -1])
+
+    elif order[0] == "back":
+        if len(queue) == 0: print(-1)
+        else: print(queue[0])
