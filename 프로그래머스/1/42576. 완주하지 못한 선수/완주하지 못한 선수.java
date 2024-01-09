9c3a5a1dd87c9f83f3e6c67bbe8e709ac63e0704
Seo Yeon Park <psycsh@naver.com>
2024-01-09 14:36:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 66.82 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        /***
+        [Sort사용해서 for문으로 비교]
+        
         Arrays.sort(participant);
         Arrays.sort(completion);
         
@@ -10,5 +13,21 @@ class Solution {
                 return participant[i];
         }
         return participant[participant.length - 1];
+        ***/
+        
+        // 해시 사용
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant)
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        for (String c : completion)
+            map.put(c, map.get(c) - 1);
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) == 1)
+                return key;
+        }
+        
+        return null;
     }
 }
