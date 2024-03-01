190a8f4aaee213ed7116108af52d93fdfc30f688
Seo Yeon Park <psycsh@naver.com>
2024-03-01 19:08:11 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int max = array[0];
+        int idx = 0;
+        
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                idx = i;
+            }
+        }
+        
+        int[] answer = {max, idx};
+        return answer;
+    }
+}
