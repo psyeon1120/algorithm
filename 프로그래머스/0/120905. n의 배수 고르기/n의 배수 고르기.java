c5bc615c782d6e42bef707a9c3faf7c44c640a5a
Seo Yeon Park <psycsh@naver.com>
2024-03-01 17:26:33 +0900

    [level 0] Title: n의 배수 고르기, Time: 2.78 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,17 +1,9 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int n, int[] numlist) {
-        int len = numlist.length;
-        int[] answer = new int[len];
-        
-        for (int i = 0; i < len; i++) {
-            if (numlist[i] % n == 0)
-                answer[i] = numlist[i];
-        }
-        
-        return Arrays.stream(answer)
-            .filter(num -> num > 0)
+    public int[] solution(int n, int[] numlist) {        
+        return Arrays.stream(numlist)
+            .filter(num -> num % n == 0)
             .toArray();
     }
 }
