8c05292bbb1c52ee5c660f0f15e262c70b875c97
Seo Yeon Park <psycsh@naver.com>
2024-03-03 20:20:58 +0900

    [level 0] Title: 중복된 문자 제거, Time: 6.69 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.split(""))
+            .distinct()
+            .collect(Collectors.joining());
+    }
+}
