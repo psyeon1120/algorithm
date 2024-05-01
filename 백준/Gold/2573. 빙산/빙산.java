b58701ac55d592acda710059ef81809842329a8f
Seo Yeon Park <psycsh@naver.com>
2024-05-02 00:36:04 +0900

    [Gold IV] Title: 빙산, Time: 480 ms, Memory: 47692 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -3,34 +3,33 @@ import java.io.*;
 
 class Main {
     static int N, M;
-    static int result = 0, cnt = -1;
     static int[][] map;
     static boolean[][] visited;
+    static int result = 0;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
-    
-    public static void meltIce(int x, int y) {
-        visited[x][y] = true;
-        int sea = 0;
-        for(int i=0; i<4; i++) {
-            int nextX = x+dx[i];
-            int nextY = y+dy[i];
-            if(nextX<0 || nextX>=N || nextY<0 || nextY>=M) continue;
-            if(!visited[nextX][nextY] && map[nextX][nextY]==0) sea++;
-        }
-        map[x][y] -= sea;
-        if(map[x][y] < 0) map[x][y] = 0;
-    }
+    static int count = 0;
     
     public static void dfs(int x, int y) {
         visited[x][y] = true;
+        meltIce(x, y);
         
-        for(int i=0; i<4; i++) {
-            int nextX = x+dx[i];
-            int nextY = y+dy[i];
-            if(nextX<0 || nextX>=N || nextY<0 || nextY>=M) continue;
-            if(!visited[nextX][nextY] && map[nextX][nextY]>0)
+        for(int i=0;i<4;i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if(nextX < 1 || nextX >= N - 1 || nextY < 1 || nextY >= M - 1) continue;
+            if(map[nextX][nextY] != 0 && !visited[nextX][nextY])
                 dfs(nextX, nextY);
+        }
+    }
+    
+    public static void meltIce(int x, int y) {
+        for(int i=0;i<4;i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
+            if(map[nextX][nextY] == 0 && !visited[nextX][nextY] && map[x][y] > 0)
+                map[x][y]--;
         }
     }
     
@@ -40,46 +39,39 @@ class Main {
         
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        
         map = new int[N][M];
+        visited = new boolean[N][M];
         
-        for(int i=0; i<N; i++) {
+        for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++) {
+            for(int j=0;j<M;j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
+        mainLoop:
         while(true) {
-            cnt = 0;
+            count = 0;
             visited = new boolean[N][M];
             
-            for(int i=1; i<N-1; i++) {
-                for(int j=1; j<M-1; j++) {
-                    if(map[i][j] > 0)
-                        meltIce(i, j);
-                }
-            }
-            
-            visited = new boolean[N][M];
-            for(int i=1; i<N-1; i++) {
-                for(int j=1; j<M-1; j++) {
-                    if(!visited[i][j] && map[i][j] > 0) {
+            for(int i=1;i<N-1;i++) {
+                for(int j=1;j<M-1;j++) {
+                    if(map[i][j]>0 && !visited[i][j]) {
+                        count++;
+                        if(count > 1) break mainLoop;
                         dfs(i, j);
-                        cnt++;
                     }
                 }
             }
-            
             result++;
-            if(cnt > 1) {
-                System.out.println(result);
-                return;
-            }
-            if(cnt == 0) {
-                System.out.println(0);
-                return;
+            
+            if(count == 0) {
+                result = 0;
+                break;
             }
         }
+        
+        System.out.println(result);
     }
+    
 }
