90d7b78cef1afd28b1a0188dbcb5f4e22755cfd7
Seo Yeon Park <psycsh@naver.com>
2024-06-17 22:46:43 +0900

    [Silver III] Title: N과 M (2), Time: 148 ms, Memory: 15728 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -6,29 +6,28 @@ class Main {
     static int[] arr;
     static StringBuilder sb = new StringBuilder();
     
-    public static void dfs(int num, int depth) {
-        if(depth == M) {
-            for(int n:arr) sb.append(n + " ");
+    public static void dfs(int num, int idx) {
+        if(idx == M) {
+            for(int n: arr) sb.append(n + " ");
             sb.append("\n");
             return;
         }
         
         for(int i=num;i<=N;i++) {
-            arr[depth] = i;
-            dfs(i+1, depth+1);
+            arr[idx] = i;
+            dfs(i+1, idx+1);
         }
     }
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        
         arr = new int[M];
         
         dfs(1, 0);
+        
         System.out.print(sb);
     }
 }
