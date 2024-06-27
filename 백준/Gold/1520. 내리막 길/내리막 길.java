650107d96dbc4c24b3ffd0c1b3487fa21ad40ed6
Seo Yeon Park <psycsh@naver.com>
2024-06-27 21:14:48 +0900

    [Gold III] Title: 내리막 길, Time: 384 ms, Memory: 34896 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,49 +1,47 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
-    static int[][] map, dp;
+public class Main {
+    static int[][] board, dp;
+    static int N, M;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
-    static int N, M, H = 0;
-    
-    public static int dfs(int x, int y) {   
-        if(x == N - 1 && y == M - 1)
-            return 1;
-        
-        if(dp[x][y] != -1) return dp[x][y];
-        
+
+    public static int dfs(int x, int y) {
+        if (x==M-1 && y==N-1) return 1;
+        if (dp[x][y] != -1) return dp[x][y];
+
         dp[x][y] = 0;
-        for(int i=0;i<4;i++) {
+
+        for (int i=0; i<4; i++) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
-            if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
-            if(map[nextX][nextY] < map[x][y]) {
+            if (nextX >=0 && nextX < M && nextY >= 0 && nextY < N
+                && board[nextX][nextY] < board[x][y]) {
                 dp[x][y] += dfs(nextX, nextY);
             }
         }
-        
         return dp[x][y];
     }
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        dp = new int[N][M];
-        
-        for(int i=0;i<N;i++) {
+        N = Integer.parseInt(st.nextToken());
+        dp = new int[M][N];
+        board = new int[M][N];
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<M;j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
                 dp[i][j] = -1;
             }
         }
-        
-        dfs(0, 0);
-        
+
+        dfs(0,0);
+
         System.out.println(dp[0][0]);
     }
 }
