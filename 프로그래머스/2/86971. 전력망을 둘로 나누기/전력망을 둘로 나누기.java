abe6ceec6258a071c1f2cbdf45fd4e4460c2bb85
Seo Yeon Park <psycsh@naver.com>
2024-09-02 19:15:06 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 14.19 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,6 +1,48 @@
+import java.util.*;
+
 class Solution {
+    boolean[][] arr;
+    int answer = 101;
+    
     public int solution(int n, int[][] wires) {
-        int answer = -1;
+        arr = new boolean[n+1][n+1];
+        
+        for(int[] wire: wires) {
+            arr[wire[0]][wire[1]] = true;
+            arr[wire[1]][wire[0]] = true;
+        }
+        
+        for(int[] wire: wires) {
+            arr[wire[0]][wire[1]] = false;
+            arr[wire[1]][wire[0]] = false;
+            int cnt = bfs(n, wire[0]);
+            answer = Math.min(answer, Math.abs(n-2*cnt));
+            arr[wire[0]][wire[1]] = true;
+            arr[wire[1]][wire[0]] = true;
+        }
+        
         return answer;
+    }
+    
+    public int bfs(int n, int idx) {
+        int cnt = 1;
+        boolean[][] visited = new boolean[n+1][n+1];
+        
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.addLast(idx);
+        
+        while(!q.isEmpty()) {
+            int now = q.removeFirst();
+            
+            for(int i=1;i<=n;i++) {
+                if(arr[now][i] && !visited[now][i]) {
+                    visited[now][i] = true;
+                    visited[i][now] = true;
+                    cnt++;
+                    q.addLast(i);
+                }
+            }
+        }
+        return cnt;
     }
 }
