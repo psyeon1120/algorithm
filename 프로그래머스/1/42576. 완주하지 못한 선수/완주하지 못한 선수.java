c1bbaddac46c0c0a906377e6705f8c784d44a2a4
Seo Yeon Park <psycsh@naver.com>
2024-09-03 13:55:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.88 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,32 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        /***
-        [Sort사용해서 for문으로 비교]
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i]))
-                return participant[i];
-        }
-        return participant[participant.length - 1];
-        ***/
-        
-        // 해시 사용
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (String p : participant)
-            map.put(p, map.getOrDefault(p, 0) + 1);
-        for (String c : completion)
-            map.put(c, map.get(c) - 1);
-        
-        for (String key : map.keySet()) {
-            if (map.get(key) == 1)
-                return key;
+        for(String p:participant) {
+            Integer temp = map.get(p);
+            if(temp!=null)
+                map.replace(p, temp+1);
+            else map.put(p, 1);
         }
         
-        return null;
+        for(String c:completion) {
+            Integer temp = map.get(c);
+            if(temp == 1) map.remove(c);
+            else map.replace(c, temp - 1);
+        }
+        
+        for(String s:map.keySet())
+            answer = s;
+        return answer;
     }
 }
